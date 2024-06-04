bc29da9faccd813dcc617211d9c961de9b6e34ad
JaimeFar94 <157666753+JaimeFar94@users.noreply.github.com>
2024-06-04 15:07:58 -0500

    ''

--- src/extension.py
+++ src/extension.py
@@ -6,23 +6,31 @@ from flask_mail import Mail
 from flask_mail import Message
 from flask_wtf.csrf import CSRFProtect  
 
-app = Flask(__name__, template_folder='templates')
-app.secret_key = os.environ.get('SECRET_KEY', '876-105-169')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app = Flask(__name__ , template_folder='templates')
+app.secret_key = '876-105-169'
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/medico'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://u3dchap7tdasok:p71f99aced8d869c27dc376cea7ce457648713c7f898a4c518c9263c625f6ccdb@c9uss87s9bdb8n.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9ccqs8833erdf'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
-# Para envío de correos
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+
+
+
+#Para envio de Correos 
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = 'farfanjaime05@gmail.com'
+app.config['MAIL_PASSWORD'] = 'olgr ogmu tavn rfue'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
-# Protección en los formularios
+
+# proteccion en los formularios
 csrf = CSRFProtect(app)
